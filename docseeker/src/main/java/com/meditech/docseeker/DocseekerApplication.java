5b7c39ee0d1ef3cb3d7e9bb26120fdf35ab68426
Wilfredo Futuri Illa <u201924361@upc.edu.pe>
2023-06-30 00:27:43 -0500

    initial commit

---
+++ docseeker/src/main/java/com/meditech/docseeker/DocseekerApplication.java
@@ -0,0 +1,13 @@
+package com.meditech.docseeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocseekerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocseekerApplication.class, args);
+	}
+
+}
---
+++ docseeker/src/test/java/com/meditech/docseeker/DocseekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditech.docseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocseekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
