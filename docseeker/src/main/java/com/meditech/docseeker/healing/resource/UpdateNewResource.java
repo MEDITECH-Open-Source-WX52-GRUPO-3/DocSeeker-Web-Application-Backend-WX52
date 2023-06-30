413b11845141a92a03aadc2fd82c9c4ad8ccef02
Wilfredo Futuri Illa <u201924361@upc.edu.pe>
2023-06-30 04:30:59 -0500

    feat: added entity New Update resource

---
+++ docseeker/src/main/java/com/meditech/docseeker/healing/resource/UpdateNewResource.java
@@ -0,0 +1,27 @@
+package com.meditech.docseeker.healing.resource;
+
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateNewResource {
+    private Long id;
+
+    @NotNull(message = "image is required")
+    private String image;
+
+    @NotBlank
+    @NotNull(message = "title is required")
+    private String title;
+    private String description;
+    private String info;
+    private Integer views;
+}
